31d42252ae257bfe4f7b62302e3f9f5466010670
AndreyShevchuk <office@shevchuk-studio.pw>
2017-09-18 07:40:48 +0000

    For  master

--- dz_lsn2.php
+++ dz_lsn2.php
@@ -1,8 +1,8 @@
 <?php
 abstract class Photographer  {
     //Action
-    public function shoot() {
-        echo "Фотограф... ";
+    public function shot() {
+        echo "Photographer  shot a ";
 		$this->performMove();
     }
 }
@@ -10,39 +10,39 @@ abstract class Photographer  {
 //start Photo objects
 class Forest extends Photographer {
     protected function performMove() {
-        echo "сфотографировал лес..." . PHP_EOL ."<br/>";
+        echo "forest..." . PHP_EOL ."";
   
     }
 }
 class Sky extends Photographer {
     protected function performMove() {
-        echo "сфотографировал небо..." . PHP_EOL ."<br/>";
+        echo "sky..." . PHP_EOL ."";
     }
     
 }
 class Man extends Photographer {
     protected function performMove() {
-        echo "сфотографировал мужчину..." . PHP_EOL ."<br/>";
+        echo "man..." . PHP_EOL ."";
     }
 }
 class Pan extends Photographer {
     protected function performMove() {
-        echo "сфотографировал ручку..." . PHP_EOL ."<br/>";
+        echo "pan..." . PHP_EOL ."";
     }
 }
 //end Photo objects
 
 //start Сall
 $photographer_action_1 = new Forest();
-$photographer_action_1->shoot();
+$photographer_action_1->shot();
 
 $photographer_action_2 = new Sky();
-$photographer_action_2->shoot();
+$photographer_action_2->shot();
 
 $photographer_action_3 = new Man();
-$photographer_action_3->shoot();
+$photographer_action_3->shot();
 
 $photographer_action_4 = new Pan();
-$photographer_action_4->shoot();
+$photographer_action_4->shot();
 //end Call
 ?>
